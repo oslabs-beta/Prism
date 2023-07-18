96748623b7bf35b21c855b126146bd090d8ccee1
Josh Hall <joshuarhall0@gmail.com>
2023-07-18 16:57:33 -0700

    EVERYTHING EVERYWHERE ALL AT ONCE

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express() ; 
+const path = require('path');
+const PORT = 3000 ; 
+
+app.use(express.json()); 
+
+// just to get something running  
+app.get('/', (req, res) => {
+    return res.status(200).send('PRISM PRISM PRISM PRISM PRISM')
+}) 
+
+// catch all route 
+app.get('*', (req, res) => {
+    return res.status(404).send('Page Not Found!');
+  });
+
+// global error handler 
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+
+//listens on port 3000 -> http://localhost:3000/
+app.listen(PORT, () => {
+    console.log(`Express listening on PORT ${PORT}`);
+  }); 
+  
