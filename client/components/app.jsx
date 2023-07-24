32368d865eb1f3145eef3f504289df27a0fcdb32
Josh Hall <joshuarhall0@gmail.com>
2023-07-24 13:07:17 -0700

    Update app.jsx

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
+import AppIntro from './AppIntro.jsx';
+import Layout from './NavBar/Layout.jsx';
+import Login from './NavBar/Login.jsx';
+import Signup from './NavBar/Signup.jsx';
+import Github from './NavBar/Github.jsx';
+import LinkedIn from './NavBar/LinkedIn.jsx';
+import ClusterConnect from './ClusterView/ClusterConnect.jsx';
 
-function App() {
+
+
+export default function App() {
+
+  const [user, setUser] = useState({});
+
   return (
-    <div className='min-h-screen bg-slate-50  items-center flex'>
-      {' '}
-      <h1 className='font-nunito text-red-500 text-4xl'>
-        Hello hellosdfsdadfdssdfsdfsdfsdf hello bye bye
-      </h1>
-    </div>
-  );
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<AppIntro />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/github" element={<Github />}></Route>
+            <Route path="/linkedin" element={<LinkedIn />}></Route>
+            {/* <Route path="/connectcluster" element={<ClusterConnect />}></Route> */}
+          </Route>
+        </Routes>
+      </div>
+    </>
+  )
 }
 
-export default App;
+
+
+ // <div>
+    //   <Routes>
+    //     <Route path="/" element={<Layout user={user} setUser={setUser} />}>
+    //       <Route index element={<AppIntro />}></Route>
+    //       <Route path='/login' element={<Login />}></Route>
+    //       <Route path='/signup' element={<Signup />}></Route>
+    //       {/* <Route path='/linkedin' element={<LinkedIn />}></Route>
+    //           <Route path='/github' element={<Github />}></Route> */}
+    //     </Route>
+    //   </Routes>
+    // </div>
