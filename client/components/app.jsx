f9dfdcaad13fb49a76b6cb7f67039a49fb6beaf6
Josh Hall <joshuarhall0@gmail.com>
2023-07-31 16:21:34 -0700

    file cleanup

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Route,
   Routes,
   createBrowserRouter,
   createRoutesFromElements,
-} from "react-router-dom";
-import AppIntro from "./AppIntro.jsx";
-import Layout from "./NavBar/Layout.jsx";
-import Login from "./Login";
-import Signup from "./Signup.jsx";
-import Github from "./NavBar/Github.jsx";
-import LinkedIn from "./NavBar/LinkedIn.jsx";
-import ClusterConnect from "./ClusterView/ClusterConnect.jsx";
+} from 'react-router-dom';
+import AppIntro from './AppIntro.jsx';
+import Layout from './NavBar/Layout.jsx';
+import Login from './NavBar/Login.jsx';
+import Signup from './NavBar/Signup.jsx';
+import Github from './NavBar/Github.jsx';
+import LinkedIn from './NavBar/LinkedIn.jsx';
+import ClusterConnect from './ClusterView/ClusterConnect.jsx';
 //import '../styles_out.css';
-import ClusterView from "./ClusterView/ClusterView.jsx";
-import RootLayout from "./ClusterView/RootLayout.jsx";
-import { ThemeProvider } from "./ClusterView//ClusterViewHeader/themeContext.js";
-import ClusterMap from "./ClusterView/Dashboard/ClusterMap.jsx";
-import NodesView from "./ClusterView//Dashboard/NodesView";
-import OverView from "./ClusterView//Dashboard/Overview";
-import PodsView from "./ClusterView//Dashboard/PodsView";
+import ClusterView from './ClusterView/ClusterView.jsx';
+import RootLayout from './ClusterView/RootLayout.jsx';
+import { ThemeProvider } from './ClusterView//ClusterViewHeader/themeContext.js';
+import ClusterMap from './ClusterView/Dashboard/ClusterMap.jsx';
+import NodesView from './ClusterView//Dashboard/NodesView';
+import OverView from './ClusterView//Dashboard/Overview';
+import PodsView from './ClusterView//Dashboard/PodsView';
 // const router = createBrowserRouter(
 //   createRoutesFromElements(
 //     <Route path = '/'
@@ -34,25 +34,25 @@ export default function App() {
       <div>
         <ThemeProvider>
           <Routes>
-            <Route path="/" element={<Layout />}>
+            <Route path='/' element={<Layout />}>
               <Route index element={<AppIntro />}></Route>
-              <Route path="/github" element={<Github />}></Route>
-              <Route path="/linkedin" element={<LinkedIn />}></Route>
+              <Route path='/login' element={<Login />}></Route>
+              <Route path='/signup' element={<Signup />}></Route>
+              <Route path='/github' element={<Github />}></Route>
+              <Route path='/linkedin' element={<LinkedIn />}></Route>
               <Route
-                path="/connectcluster"
+                path='/connectcluster'
                 element={<ClusterConnect />}
               ></Route>
             </Route>
 
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
-            <Route path="/clusterview" element={<RootLayout />}>
+            <Route path='/clusterview' element={<RootLayout />}>
               <Route index element={<OverView />} />
-              <Route exact path="/clusterview/nodes" element={<NodesView />} />
-              <Route exact path="/clusterview/pods" element={<PodsView />} />
+              <Route exact path='/clusterview/nodes' element={<NodesView />} />
+              <Route exact path='/clusterview/pods' element={<PodsView />} />
               <Route
                 exact
-                path="/clusterview/clustermap"
+                path='/clusterview/clustermap'
                 element={<ClusterMap />}
               />
             </Route>
