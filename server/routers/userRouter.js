712666dff794042cfcf89930c58c249ed0f2b522
Josh Hall <joshuarhall0@gmail.com>
2023-07-31 16:19:48 -0700

    Co-authored-by: Beserat Tafesse <BeseratT@users.noreply.github.com> Co-authored-by: Dawit Merid <dawitmerid@users.noreply.github.com>

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-const express = require('express');
-
-// right now this is empty — do we need it? 
-=======
-import { Router } from 'express';
-import { createUser, authUser } from '../controllers/userController.js';
+import { Router } from "express";
+import { createUser, authUser } from "../controllers/userController.js";
 const router = Router();
 
-router.post('/signup', createUser, (req, res) => res.json(res.locals.user));
+router.post("/signup", createUser, (req, res) => res.json(res.locals.user));
 
-router.get('/login', authUser, (req, res) => {
+router.get("/login", authUser, (req, res) => {
   res.json(res.locals.userAuth);
 });
->>>>>>> dev
