3d701d3da65ec52c42a5c82eac6729dfd8b3adfc
Jxmes-Li <123266816+Jxmes-Li@users.noreply.github.com>
2023-08-24 09:45:16 -0400

    nothing

--- client/components/AppIntro.jsx
+++ client/components/AppIntro.jsx
@@ -7,6 +7,7 @@ export default function AppIntro() {
             <div>  | </div>
             <div>  | </div>
             <div>  | </div>
+            <div>  |  </div>
             <h1>This is start of landing page</h1>
             <div>Welcome to Prism</div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloribus at odio neque consequatur autem impedit sed expedita est dolore amet quod quo debitis quae reiciendis temporibus optio, totam placeat.</p>
