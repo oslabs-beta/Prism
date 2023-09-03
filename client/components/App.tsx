e5bf0a1007eabf0a1dded629b9a6bfa73d73f328
Paul Glenn <paglenn@users.noreply.github.com>
2023-09-03 18:34:15 -0400

    comments/cleaning, prep to remove cookies from js access

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,10 @@
-import React, { useState, FC, useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import Login from './Login';
-import Signup from './Signup';
-// import LandingPage from './LandingPage/LandingPage';
+import React, { useState, FC, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
 
-import { ThemeProvider } from './ClusterView/ClusterViewHeader/themeContext';
-import Dashboard from './ClusterView/Dashboard';
+import { ThemeProvider } from "./ClusterView/ClusterViewHeader/themeContext";
+import Dashboard from "./ClusterView/Dashboard";
 
 interface Props {}
 
@@ -18,13 +17,13 @@ const App: FC<Props> = () => {
   useEffect(() => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const codeParam = urlParams.get('code');
+    const codeParam = urlParams.get("code");
     console.log(codeParam);
 
-    if (codeParam && localStorage.getItem('accessToken') === null) {
+    if (codeParam && localStorage.getItem("accessToken") === null) {
       async function getAccessToken() {
-        await fetch('/user/getAccessToken?code=' + codeParam, {
-          method: 'GET',
+        await fetch("/user/getAccessToken?code=" + codeParam, {
+          method: "GET",
         })
           .then((response) => {
             return response.json();
@@ -33,10 +32,9 @@ const App: FC<Props> = () => {
             console.log(data);
 
             if (data.access_token) {
-              console.log('test from app.tx, then statement');
-              localStorage.setItem('accessToken', data.access_token);
+              localStorage.setItem("accessToken", data.access_token);
               setRerender(!rerender);
-              navigate('/dashboard');
+              navigate("/dashboard");
             }
           });
       }
@@ -47,29 +45,11 @@ const App: FC<Props> = () => {
   return (
     <>
       <div>
-        <ThemeProvider initialTheme={'dark'}>
+        <ThemeProvider initialTheme={"dark"}>
           <Routes>
-            <Route path='/' element={<Login />} />
-            {/* <Route path='/login' element={<Login />} /> */}
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/dashboard' element={<Dashboard />} />
-
-            {/* <Route index element={<AppIntro />}></Route>
-              <Route path='/login' element={<Login />}></Route>
-              <Route path='/signup' element={<Signup />}></Route>
-              <Route path='/github' element={<Github />}></Route>
-              <Route path='/linkedin' element={<LinkedIn />}></Route>
-              <Route
-                path='/connectcluster'
-                element={<ClusterConnect />}
-              ></Route>
-            </Route>
-            <Route path='/clusterview' element={<OverView />}> */}
-            {/* <Route index element={<OverView />} /> */}
-            {/* <Route exact path='/clusterview/nodes' element={<NodesView />} />
-              <Route exact path='/clusterview/pods' element={<PodsView />} />
-              <Route exact path='/clusterview/clustermap' element={<ClusterMap />} /> */}
-            {/* </Route> */}
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </ThemeProvider>
       </div>
