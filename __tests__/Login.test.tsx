68422e683c44dbf08c4876b5a86571106c21ce9c
Paul Glenn <paglenn@users.noreply.github.com>
2023-09-18 11:22:23 -0400

    added frontend test for oauth

--- __tests__/Login.test.tsx
+++ __tests__/Login.test.tsx
@@ -1,44 +1,56 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Login from '../client/components/Login';
-import { beforeEach, describe, expect, jest, test } from '@jest/globals';
-import { useNavigate } from 'react-router';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Login from "../client/components/Login";
+import { beforeEach, describe, expect, jest, test } from "@jest/globals";
 // to import Jest and mocking useNavigate
+import "@testing-library/jest-dom";
+import { ThemeProvider } from "../client/components/ClusterView/ClusterViewHeader/themeContext";
+import { Routes, Route, BrowserRouter, HashRouter } from "react-router-dom"; // ne
 const mockUsedNavigate = jest.fn();
 
-jest.mock('react-router-dom', () => ({
-  useNavigate: () => mockUsedNavigate,
-}));
+// jest.mock("react-router-dom", () => ({
+//   useNavigate: () => mockUsedNavigate,
+// }));
 
-// const useNavigateMock = () => {
-//     return mockUsedNavigate
-// }
-
-// jest.mock('react-router-dom', () => {
-//     useNavigate: () => mockUsedNavigate
-// })
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
 
 // Renders Login component before each test
 beforeEach(() => {
-  render(<Login />);
+  render(
+    <ThemeProvider initialTheme={"dark"}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Login />} path="/" />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 });
 
-test('If it renders', () => {
+test("If it renders", () => {
   const ha = screen.getByText(/Login Form/);
+  // screen.debug();
   expect(ha).toBeInTheDocument();
 });
 
-test('If username and password input bars render', () => {
-  const username = screen.getByRole('textbox', {
-    name: 'username',
+test("If username and password input bars render", () => {
+  const username = screen.getByRole("textbox", {
+    name: "username",
   });
   const password = screen.getByLabelText(/password/);
   expect(username).toBeInTheDocument();
   expect(password).toBeInTheDocument();
 });
-
-test('If clicking close goes to dashboard', () => {});
-// function beforeEach(arg0: () => void) {
-//     throw new Error("Function not implemented.");
-// }
