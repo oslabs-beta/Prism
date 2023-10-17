c08de4fe312390d26b43654b07bede25494f4377
Paul Glenn <paglenn@users.noreply.github.com>
2023-10-17 14:31:47 -0400

    added run-tests action

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { HashRouter, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import { createRoot } from "react-dom/client";
 
